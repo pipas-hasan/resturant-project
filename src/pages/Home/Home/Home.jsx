8440b3f4ad080ab46992971f1032d04faacab420
pipas hasan <pipasrajbari@gmail.com>
2023-11-15 17:18:06 +0600

    our menu section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import PopulerMenu from "../../PopulerMenu/PopulerMenu";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <Banner/>
       <Category/>
+      <PopulerMenu/>
     </div>
   );
 }
---
+++ src/pages/PopulerMenu/PopulerMenu.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import MenuItem from "../Shared/MenuItem/MenuItem";
+
+const PopulerMenu = () => {
+const [menu, setMenu] = useState([]);
+    useEffect(() => {
+      fetch('menu.json')
+      .then(res => res.json())
+      .then(data => {
+        const popularItems = data.filter(item => item.category === "popular");
+         setMenu(popularItems)} )
+    }, []);
+
+
+  return (
+    <div className="max-w-6xl mx-auto my-12">
+       <SectionTitle heading={"FROM OUR MENU"} subHeading={"Check it out"}></SectionTitle>
+    <div className="grid grid-cols-2 gap-4">
+        {
+            menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+        }
+    </div>
+    </div>
+  );
+}
+
+export default PopulerMenu;
