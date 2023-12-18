c98087a6871fcd5c913087fa7583b96745387524
pipas hasan <pipasrajbari@gmail.com>
2023-12-18 12:24:06 +0600

    pagination page

--- src/pages/order/order/OrderTab.jsx
+++ src/pages/order/order/OrderTab.jsx
@@ -1,13 +1,37 @@
 /* eslint-disable react/prop-types */
-
 import FoodCard from "../../../components/FoodCard/FoodCard";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const OrderTab = ({items}) => {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from 'swiper/modules';
+
+const OrderTab = ({ items }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
+
   return (
-    <div className="grid grid-cols-3 gap-8">
-            {items.map(item => <FoodCard key={item._id} item={item}></FoodCard>)}
-        </div>
+    <div>
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="grid grid-cols-3 gap-8">
+            {items.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
-}
+};
 
 export default OrderTab;
